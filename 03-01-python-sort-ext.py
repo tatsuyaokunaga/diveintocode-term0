38658180c60e72a50c362e6b4e808cec8b2ffe4d
okunagatatsuya <aminao4000@gmail.com>
2018-04-04 11:53:42 +0900

    AIF_Python演習_1作成

---
+++ 03-01-python-sort-ext.py
@@ -0,0 +1,11 @@
+import csv
+#csvモジュールをインポートする
+
+f = open('KOUKYOU.csv', 'r') #ファイルオブジェクトとしてファイルを開く
+dataReader = csv.reader(f) #与えられた csvfile 内の行を反復処理するような reader オブジェクトを返す
+header = next(dataReader)  # 最初の一行をヘッダーとして取得
+data=sorted(dataReader,key=lambda x:x[1]) #昇順に並び替える
+print(' '.join(header))  # ヘッダーをスペース区切りで表示
+for row in data: #dataは、1行ずつをリストとして返す
+    mojiretsu=' '.join(row) # リスト内の各要素を半角スペースで連結
+    print(mojiretsu)
---
+++ 03-01-python-sort.py
@@ -0,0 +1,9 @@
+import csv
+#csvモジュールをインポートする
+
+f = open('KOUKYOU.csv', 'r') #ファイルオブジェクトとしてファイルを開く
+dataReader = csv.reader(f) #与えられた csvfile 内の行を反復処理するような reader オブジェクトを返す
+
+for row in dataReader:　#dataReaderは、1行ずつをリストとして返す
+   mojiretsu=' '.join(row)　# リスト内の各要素を半角スペースで連結
+   print(mojiretsu)
