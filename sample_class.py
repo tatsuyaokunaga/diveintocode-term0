6124f0766a7b9b99110042cdcf44e9c7fc36ea2b
okunagatatsuya <aminao4000@gmail.com>
2018-04-07 15:20:11 +0900

    インスタンスメソッド課題

--- sample_class.py
+++ sample_class.py
@@ -1,7 +1,7 @@
 import webbrowser
 
 class School():
-    def __init__(self,name,address,number_of_students,founding_year,\
+    def __init__(self,name,address,number_of_students,founding_year,\  #コンストラクタでインスタンス変数を定義
            introduction_video_url,introduction_statement):
         self.name = name
         self.address = address
@@ -10,14 +10,14 @@ class School():
         self.introduction_video_url = introduction_video_url
         self.introduction_statement = introduction_statement
 
-    def sample_instance_method(self):
+    def sample_instance_method(self): #インスタンスメソッドを定義
         print("{}。そして、{}は、{}にあり創立から{}年の学校で、生徒は{}人です。".format(self.introduction_statement,\
              self.name,self.address,self.founding_year,self.number_of_students))
         webbrowser.open(self.introduction_video_url)
 
-
+# A学校用のオブジェクトを作成
 a_school = School("A学校","渋谷区",300,100,'https://www.youtube.com/watch?v=fizvBkVYdso','A学校は自然豊かな...')
 a_school.sample_instance_method()
-
+# B学校用のオブジェクトを作成
 b_school = School("B学校", "東京都新宿区..", 500, 30, "https://www.youtube.com/watch?v=wmfLQMKiwWI&t=311s", "B学校は文武両立で...")
 b_school.sample_instance_method()
